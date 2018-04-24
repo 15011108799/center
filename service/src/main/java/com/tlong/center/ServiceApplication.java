688d220b0c2542370766c392eacf8e4feaee85d4
francis <50712792@qq.com>
2018-04-24 15:50:39 +0800

    new

--- service/src/main/java/com/tlong/center/ServiceApplication.java
+++ service/src/main/java/com/tlong/center/ServiceApplication.java
@@ -1,23 +1,27 @@
 package com.tlong.center;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.ApplicationContext;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@SpringBootApplication
-//@EnableFeignClients
-//@EnableSwagger2
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableSwagger2
-//@EnableAutoConfiguration
-public class ServiceApplication {
+public class ServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder builder) {
+// TODO Auto-generated method stub
+		return builder.sources(this.getClass());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
