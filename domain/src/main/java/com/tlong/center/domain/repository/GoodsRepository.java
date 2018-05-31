e01db6441ad623c6e6a25166144e6bdbddec1701
libin111 <38640410+libin111@users.noreply.github.com>
2018-05-31 13:51:15 +0800

    商品列表增删改查 取消审核功能

--- domain/src/main/java/com/tlong/center/domain/repository/GoodsRepository.java
+++ domain/src/main/java/com/tlong/center/domain/repository/GoodsRepository.java
@@ -2,5 +2,5 @@ package com.tlong.center.domain.repository;
 
 import com.tlong.center.domain.app.goods.WebGoods;
 
-public interface AppGoodsRepository extends BaseRepository<WebGoods,Long>{
+public interface GoodsRepository extends BaseRepository<WebGoods,Long> {
 }
--- service/src/main/java/com/tlong/center/ServiceApplication.java
+++ service/src/main/java/com/tlong/center/ServiceApplication.java
@@ -3,11 +3,18 @@ package com.tlong.center;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.servlet.MultipartConfigElement;
 
 
 @SpringBootApplication
@@ -16,14 +23,35 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class ServiceApplication extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(
-			SpringApplicationBuilder builder) {
-// TODO Auto-generated method stub
-		return builder.sources(this.getClass());
-	}
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //允许上传的文件最大值
+        factory.setMaxFileSize("50MB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("50MB");
+        return factory.createMultipartConfig();
+    }
+
+    @Bean
+    EmbeddedServletContainerCustomizer containerCustomizer() throws Exception {
+        return (ConfigurableEmbeddedServletContainer container) -> {
+            if (container instanceof TomcatEmbeddedServletContainerFactory) {
+                TomcatEmbeddedServletContainerFactory tomcat = (TomcatEmbeddedServletContainerFactory) container;
+                tomcat.addConnectorCustomizers((connector) -> {
+                    connector.setMaxPostSize(50000000); // 50 MB
+                });
+            }
+        };
+    }
 }
