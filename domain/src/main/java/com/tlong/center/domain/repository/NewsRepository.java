cf00ecc163e30a6f2005046c2aa611bc76cb5a73
libin111 <38640410+libin111@users.noreply.github.com>
2018-06-01 15:14:25 +0800

    新闻增删改查 取消审核功能

---
+++ domain/src/main/java/com/tlong/center/domain/repository/NewsRepository.java
@@ -0,0 +1,7 @@
+package com.tlong.center.domain.repository;
+
+
+import com.tlong.center.domain.web.WebNews;
+
+public interface NewsRepository extends BaseRepository<WebNews, Long> {
+}
---
+++ domain/src/main/java/com/tlong/center/domain/web/WebNews.java
@@ -0,0 +1,138 @@
+package com.tlong.center.domain.web;
+
+import com.tlong.center.api.dto.news.NewsRequestDto;
+import com.tlong.core.utils.PropertyUtils;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tlong_news")
+@DynamicUpdate
+public class WebNews {
+
+    public WebNews() {
+
+    }
+
+    public WebNews(NewsRequestDto dto) {
+        PropertyUtils.copyPropertiesOfNotNull(dto, this);
+    }
+
+    public NewsRequestDto toDto() {
+        NewsRequestDto dto = new NewsRequestDto();
+        PropertyUtils.copyPropertiesOfNotNull(this, dto);
+        return dto;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    //标题
+    private String title;
+
+    //标题图标
+    private String titleIcon;
+
+    //简介
+    private String des;
+
+    //内容
+    private String content;
+
+    //图片
+    private String pic;
+
+    //视频
+    private String video;
+
+    //发布人
+    private String publishName;
+
+    //是否审核
+    private String isCheck;
+
+    //发布时间
+    private String publishTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitleIcon() {
+        return titleIcon;
+    }
+
+    public void setTitleIcon(String titleIcon) {
+        this.titleIcon = titleIcon;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public String getPublishName() {
+        return publishName;
+    }
+
+    public void setPublishName(String publishName) {
+        this.publishName = publishName;
+    }
+
+    public String getIsCheck() {
+        return isCheck;
+    }
+
+    public void setIsCheck(String isCheck) {
+        this.isCheck = isCheck;
+    }
+
+    public String getPublishTime() {
+        return publishTime;
+    }
+
+    public void setPublishTime(String publishTime) {
+        this.publishTime = publishTime;
+    }
+}
