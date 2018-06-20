b2797d463f67d2370f8361383f744b32220124da
francis <50712792@qq.com>
2018-06-20 16:04:23 +0800

    null

--- api/src/main/java/com/tlong/center/api/client/fallback/WebLoginClientFallback.java
+++ api/src/main/java/com/tlong/center/api/client/fallback/WebLoginClientFallback.java
@@ -3,10 +3,13 @@ package com.tlong.center.api.client.fallback;
 import com.tlong.center.api.client.web.WebLoginClient;
 import com.tlong.center.api.dto.web.WebLoginRequestDto;
 import com.tlong.center.api.dto.web.WebLoginResponseDto;
+import feign.hystrix.FallbackFactory;
 
-public class WebLoginClientFallback implements WebLoginClient {
+import javax.servlet.http.HttpSession;
+
+public class WebLoginClientFallback implements FallbackFactory<WebLoginClientFallback> {
     @Override
-    public WebLoginResponseDto webLogin(WebLoginRequestDto requestDto) {
+    public WebLoginClientFallback create(Throwable throwable) {
         return null;
     }
 }
